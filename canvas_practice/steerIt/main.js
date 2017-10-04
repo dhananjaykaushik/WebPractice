49f115084e82189387490e3499c4247c135221fb
Dhananjay Kaushik <dhananjaykaushik96@gmail.com>
2017-10-05 02:25:37 +0530

    done

--- canvas_practice/steerIt/main.js
+++ canvas_practice/steerIt/main.js
@@ -1,5 +1,5 @@
 var font;
-
+var vehicles = [];
 
 function preload() {
     
@@ -12,15 +12,30 @@ function setup() {
     textFont(font);
     background(0);
     
-    var points = font.textToPoints("DHANANJAY", width /80, height / 1.5, 200);
+    var points = font.textToPoints("HELLO", width / 5, height / 1.5, 200);
     
     for(var i = 0; i < points.length; ++i) {
         var pt = points[i];
-        stroke(255);
-        strokeWeight(8);
-        point(pt.x, pt.y);
+        vehicles.push(new Vehicle(pt.x, pt.y));
+        
     }
     
 }
 
-function draw() {}
+function draw() {
+    
+    background(0);
+    
+    for(var i = 0; i < vehicles.length; ++i) {
+        
+        var v = vehicles[i];
+        
+        v.behaviors();
+        v.update();
+        v.show();
+        
+        
+    }
+    
+    
+}
